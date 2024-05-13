b5875215a410fe66cae31746d82f4f15e2c85aee
Doris-Mwito5 <kawiramwito5@gmail.com>
2024-05-13 12:17:30 +0300

    added list() method handles GET requests to list customers

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+from urllib import response
 from django.shortcuts import render
+from rest_framework.response import Response
 from .models import Customer, Profession, Datasheet, Document
 from rest_framework import viewsets
 from .serializer import CustomerSerializer, ProfessionSerializer, DatasheetSerializer, DocumentSerializer 
@@ -8,7 +10,13 @@ class CustomerViewSet(viewsets.ModelViewSet):
         active_customers = Customer.objects.filter(active=True)
         return active_customers
     
-    serializer_class = CustomerSerializer
+    
+    def list(self, request, *args, **kwargs):
+        import pdb; pdb.set_trace()
+        customers = Customer.objects.filter(id=3)
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+        
     
 class ProfessionViewset(viewsets.ModelViewSet):
     queryset = Profession.objects.all()
